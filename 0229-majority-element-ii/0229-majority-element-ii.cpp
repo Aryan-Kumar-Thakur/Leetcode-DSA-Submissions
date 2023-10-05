b7e7698a6a1fbeba5cbee083955b1cd59c6ed5fe
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-10-05 11:12:15 +0530

    Time: 6 ms (90%), Space: 16.2 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int cnt1=0,cnt2=0;
+        int x=INT_MAX,y=INT_MAX;
         vector<int> ans;
         int n=nums.size();
-        map<int,int> mp;
-        int x=n/3;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+        for(auto it:nums){
+            if(cnt1==0 && y!=it){
+                x=it;
+                cnt1++;
+            }
+            else if(cnt2==0 && x!=it){
+                y=it;
+                cnt2++;
+            }
+            else if(x==it){
+                cnt1++;
+            }
+            else if(y==it){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
-        for(auto it:mp){
-            if(it.second > x){
-                ans.push_back(it.first);
+        cnt1=0,cnt2=0;
+        int val=(n/3)+1;
+        for(auto it:nums){
+            if(x==it){
+                cnt1++;
+            }
+            if(y==it){
+                cnt2++;
+            }
+            if(cnt1==val){
+                ans.push_back(it);
+                cnt1=0;
+                x=INT_MAX;
+            }
+            if(cnt2==val){
+                ans.push_back(it);
+                cnt2=0;
+                y=INT_MAX;
             }
         }
         return ans;
