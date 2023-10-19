d67461f39a5ff073315e674f5823d4912a10daac
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-10-19 18:36:11 +0530

    Time: 0 ms (100%), Space: 6.5 MB (54.78%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st;
-        string ans1="",ans2="";
         int n=s.size(),m=t.size();
-        for(int i=0;i<n;i++){
-            if(s[i]=='#' && !st.empty()){
-                st.pop();
+        int i=n-1,j=m-1;
+        int hash1=0,hash2=0;
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s[i]=='#'){
+                    hash1++;
+                    i--;
+                }
+                else if(hash1 > 0){
+                    hash1--;
+                    i--;
+                }
+                else{
+                    break;
+                }
             }
-            else if(s[i]!='#'){
-                st.push(s[i]);
+            while(j>=0){
+                if(t[j]=='#'){
+                    hash2++;
+                    j--;
+                }
+                else if(hash2>0){
+                    hash2--;
+                    j--;
+                }
+                else{
+                    break;
+                }
+            }
+            char c1= (i < 0)? '$' : s[i];
+            char c2= (j < 0)? '$' : t[j];
+            if(c1!=c2){
+                return false;
+            }
+            else{
+                i--;
+                j--;
             }
         }
-        while(!st.empty()){
-            ans1+=st.top();
-            st.pop();
-        }
-        for(int i=0;i<m;i++){
-            if(t[i]=='#' && !st.empty()){
-                st.pop();
-            }
-            else if(t[i]!='#'){
-                st.push(t[i]);
-            }
-        }
-        while(!st.empty()){
-            ans2+=st.top();
-            st.pop();
-        }
-        return (ans1==ans2);
+        return (i<0 && j<0);
     }
 };
