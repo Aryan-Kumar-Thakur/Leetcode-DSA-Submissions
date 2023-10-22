6c19c6b38579365c9dae86bf313618764ebecaa6
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-10-22 12:26:26 +0530

    Time: 103 ms (98.68%), Space: 89.7 MB (73.03%) - LeetHub

--- 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -1,44 +1,22 @@
 class Solution {
-    void findprevsmallest(vector<int> &left,vector<int> &nums,stack<int> &st){
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                left[i]=st.top();
-            }
-            st.push(i);
-        }
-    }
-    void findnextsmallest(vector<int> &right,vector<int> &nums,stack<int> &st){
-        int n=nums.size();
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                right[i]=st.top();
-            }
-            st.push(i);
-        }
-    }
 public:
     int maximumScore(vector<int>& nums, int k) {
+        int l=k,r=k;
         int n=nums.size();
-        vector<int> left(n,-1);
-        vector<int> right(n,n);
-        stack<int> st;
-        findprevsmallest(left,nums,st);
-        while(!st.empty()){
-            st.pop();
-        }
-        findnextsmallest(right,nums,st);
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(left[i]<k && right[i]>k){
-                ans=max(ans,nums[i]*((right[i]-left[i])-1));
+        int mini=nums[k],ans=nums[k];
+
+        while(l>0 || r<n-1){
+            int left = (l > 0) ? nums[l-1]:0; 
+            int right = (r < n-1) ? nums[r+1]:0;
+            if(left > right){
+                l--;
+                mini=min(mini,left);
             }
+            else{
+                r++;
+                mini=min(mini,right);
+            }
+            ans=max(ans,mini*(r-l+1));
         }
         return ans;
     }
