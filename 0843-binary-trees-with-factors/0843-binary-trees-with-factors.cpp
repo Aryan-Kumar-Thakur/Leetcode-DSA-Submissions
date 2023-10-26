4a1d66d318a23b3e03e8c569f6a085a4a51751a3
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-10-26 10:32:27 +0530

    Time: 21 ms (95.28%), Space: 10.1 MB (36.22%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
@@ -10,7 +10,6 @@ public:
         for(auto it:arr){
             cnt[it]=1;
         }
-
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(arr[j] > sqrt(arr[i])) break;
