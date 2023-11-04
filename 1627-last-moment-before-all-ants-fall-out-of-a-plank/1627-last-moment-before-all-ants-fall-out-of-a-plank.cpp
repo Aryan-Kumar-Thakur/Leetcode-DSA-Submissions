f7cfa9b064cde9752c11809f7eb876ddaf3ead1f
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-11-04 12:14:10 +0530

    Time: 11 ms (97.41%), Space: 23.1 MB (31.47%) - LeetHub

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -4,28 +4,11 @@ public:
         int ans=INT_MIN;
         int N=left.size();
         int M=right.size();
-        sort(left.begin(),left.end());
-        sort(right.begin(),right.end());
-        if(N==0){
-            return (n-right[0]);
-        }
-        if(M==0){
-            return (left[N-1]-0);
-        }
         for(int i=0;i<N;i++){
-            int res=INT_MIN;
-            int x=left[i]-0;
-            for(int j=0;j<M;j++){
-                int y=n-right[j];
-                int z=INT_MIN;
-                if((left[i] > right[j]) && (left[i]-right[j]) % 2 == 0){
-                    int l=(left[i]-right[j])/2;
-                    z=max(z,abs(l-left[i] + n-left[i]));
-                    z=max(z,abs(l-right[j] + right[j]-0));
-                }
-                res=max(res,max(max(x,y),z));
-            }
-            ans=max(ans,res);
+            ans=max(ans,left[i]-0);
+        }
+        for(int i=0;i<M;i++){
+            ans=max(ans,n-right[i]);
         }
         return ans;
     }
