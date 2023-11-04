720161c7cb1072d2c689963a82f80b95aa7ad914
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-11-04 19:34:21 +0530

    Time: 0 ms (100%), Space: 6.7 MB (67.77%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     string fractionToDecimal(int numerator, int denominator) {
-      if (numerator == 0)
+        if(numerator==0){
             return "0";
-
+        }
         string ans;
-        if ((numerator > 0) ^ (denominator > 0))
-            ans += "-";
-        // cout << "ans : " << ans << endl;
-
-        long n = labs(numerator);
-        long d = labs(denominator);
-        long r = n % d;
-        ans += to_string(n / d);
-        // cout << "ans : " << ans << endl;
-
-        if (r == 0)
+        if((numerator > 0) ^ (denominator > 0)){
+            ans+='-';
+        }
+        long n=labs(numerator);
+        long d=labs(denominator);
+        long r = (n%d);
+        ans+=to_string(n/d);
+        if(r==0){
             return ans;
-
-        ans += ".";
-        // cout << "ans : " << ans << endl;
-        unordered_map<long, int> m;
-        while (r)
-        {
-            // cout << "rem : " << r << endl;
-            if (m.find(r) != m.end())
-            {
-                ans.insert(m[r], "(");
-                ans += ")";
-                // cout << "ans : " << ans << endl;
+        }
+        ans+='.';
+        unordered_map<int,int> mp;
+        while(r){
+            if(mp.find(r)!=mp.end()){
+                ans.insert(mp[r],"(");
+                ans+=")";
                 break;
             }
-
-            m[r] = ans.size();
-            // cout << "m[r] : " << m[r] << endl;
-            r = r * 10;
-            ans += to_string(r / d);
-            // cout << "ans : " << ans << endl;
-            r = r % d;
+            mp[r]=ans.size();
+            r=r*10;
+            ans+=to_string(r/d);
+            r=r % d;
         }
         return ans;
     }
