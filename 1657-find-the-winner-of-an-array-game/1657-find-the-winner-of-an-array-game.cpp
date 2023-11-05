5b89a4d80c300c7af945ab03a838b7bd9cedebf6
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2023-11-05 11:16:56 +0530

    Time: 96 ms (30.56%), Space: 63.7 MB (29.63%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -1,41 +1,21 @@
 class Solution {
-    void findnextgreatest(vector<int> &arr,vector<int> &right,stack<int> &st,int n){
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()] < arr[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                right[i]=st.top();
-            }
-            st.push(i);
-        }
-    }
 public:
     int getWinner(vector<int>& arr, int k) {
-        map<int,int> mp;
-        int ans=-1;
+        int j=0;
+        int win=0;
         int n=arr.size();
-        stack<int> st;
-        vector<int> right(n,n);
-        findnextgreatest(arr,right,st,n);
-        int i=0;
-        if(arr[0] < arr[1]){
-            i=1;
-        }
-        while(i<n){
-            int win=arr[i];
-            if(i>0){
-                mp[win]=1;
+        for(int i=1;i<n;i++){
+            if(arr[j] > arr[i]){
+                win++;
             }
-            if(right[i]==n){
-                return arr[i];
+            else{
+                win=1;
+                j=i;
             }
-            mp[win]+=(right[i]-(i+1));
-            i=right[i];
-            if(mp[win]>=k){
-                return win;
+            if(win>=k){
+                return arr[j];
             }
         }
-        return -1;
+        return arr[j];
     }
 };
