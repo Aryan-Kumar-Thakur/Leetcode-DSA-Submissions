dd05fe451c80d6a4f8ebc97043f5a1a310c650bd
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-07-12 14:09:17 +0530

    Time: 59 ms (91.87%), Space: 18.2 MB (75.61%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -2,53 +2,36 @@ class Solution {
 public:
     int maximumGain(string s, int x, int y) {
         int n=s.size();
-        stack<char> st1;
-        stack<char> st2;
-        int ans1=0;
-        int ans2=0;
-        if(x>=y){
-            for(int i=0;i<n;i++){
-                if(!st1.empty() && st1.top()=='a' && s[i]=='b'){
-                    st1.pop();
-                    ans1+=x;
+        int acnt=0;
+        int bcnt=0;
+        int mini=min(x,y);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]>'b'){
+                ans+=min(acnt,bcnt)*mini;
+                acnt=0;
+                bcnt=0;
+            }
+            else if(s[i]=='a'){
+                if(y>x && bcnt > 0){
+                    bcnt--;
+                    ans+=y;
                 }
                 else{
-                    st1.push(s[i]);
+                    acnt++;
                 }
             }
-            while(!st1.empty()){
-                if(!st2.empty() && st2.top()=='a' && st1.top()=='b'){
-                    st2.pop();
-                    ans1+=y;
+            else{
+                if(x>y && acnt>0){
+                    acnt--;
+                    ans+=x;
                 }
                 else{
-                    st2.push(st1.top());
+                    bcnt++;
                 }
-                st1.pop();
-            }
-
-        }
-        else{
-            for(int i=0;i<n;i++){
-                if(!st1.empty() && st1.top()=='b' && s[i]=='a'){
-                    st1.pop();
-                    ans2+=y;
-                }
-                else{
-                    st1.push(s[i]);
-                }
-            }
-            while(!st1.empty()){
-                if(!st2.empty() && st2.top()=='b' && st1.top()=='a'){
-                    st2.pop();
-                    ans2+=x;
-                }
-                else{
-                    st2.push(st1.top());
-                }
-                st1.pop();
             }
         }
-        return max(ans1,ans2);
+        ans+=min(acnt,bcnt)*mini;
+        return ans;
     }
 };
