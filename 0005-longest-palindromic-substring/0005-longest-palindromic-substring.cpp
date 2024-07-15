4643e8722818ca50cf8e507d9d377306419a9480
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-07-16 00:44:25 +0530

    Time: 11 ms (84.66%), Space: 8.5 MB (73.3%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,51 +1,28 @@
-class Solution
-{
-    public:
-        string longestPalindrome(string s)
-        {
-            int ini = 0, len = INT_MIN;
-            int n = s.size();
-            int lo,hi;
-            for (int i = 0; i < n; i++)
-            {
-             lo=i-1;
-             hi=i+1;
-            while(lo>=0 && s[i]==s[lo]){
-                lo--;
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int ind=0;
+        int sind=0;
+        int maxlen=0;
+        while(ind<n){
+            int left=ind,right=ind;
+            while(left>=0 && s[left]==s[ind]){
+                left--;
             }
-            while(hi<n && s[i]==s[hi]){
-                hi++;
+            while(right<n && s[right]==s[ind]){
+                right++;
             }
-            while(lo>=0 && hi<n && s[lo]==s[hi]){
-                lo--;
-                hi++;
+            while(left>=0 && right<n && s[left]==s[right]){
+                left--;
+                right++;
             }
-            if(hi-lo-1 > len){
-                len=hi-lo-1;
-                ini=lo+1;
+            if(right-left-1 > maxlen){
+                maxlen=right-left-1;
+                sind=left+1;
             }
+            ind++;
         }
-            //     l = i - 1;
-            //     r = i + 1;
-            //     while (l >= 0 && s[i] == s[l])
-            //     {
-            //         l--;
-            //     }
-            //     while (r < n && s[r] == s[i])
-            //     {
-            //         r++;
-            //     }
-            //     while (l >= 0 && r < n && s[l] == s[r])
-            //     {
-            //         l++;
-            //         r++;
-            //     }
-            //     if ((r - l - 1) > len)
-            //     {
-            //         len = (r - l - 1);
-            //         ini = l + 1;
-            //     }
-
-            return s.substr(ini,len);
-        }
+        return s.substr(sind,maxlen);
+    }
 };
