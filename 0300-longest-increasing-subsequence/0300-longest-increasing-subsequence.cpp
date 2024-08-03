29642f9ffe2007aeff2de013c11198c2488c8e90
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-08-03 19:37:36 +0530

    Time: 4 ms (95.14%), Space: 13.1 MB (62.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    void solve(int ind,int n,vector<int>& nums,vector<int>& ans){
-        if(ind==n){
-            return;
-        }
-        if(ans.size()==0 || nums[ind]>ans.back()){
-            ans.push_back(nums[ind]);
-        }
-        else{
-            int idx=lower_bound(ans.begin(),ans.end(),nums[ind])-ans.begin();
-            ans[idx]=nums[ind];
-        }
-        solve(ind+1,n,nums,ans);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<int> ans;
-        solve(0,n,nums,ans);
+        for(int i=0;i<n;i++){
+            if(ans.size()==0 || nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                int idx=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[idx]=nums[i];
+            }
+        }
         return ans.size();
     }
 };
