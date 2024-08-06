57d10d0a179b9523764852e50d4257b373a14929
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-08-07 03:00:28 +0530

    Time: 49 ms (92.95%), Space: 14 MB (84.66%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -10,23 +10,25 @@ public:
             return 0;
         }
         int target=sum/2;
-        vector<vector<int>> dp(n,vector<int>(target+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
-        }
+        // vector<vector<int>> dp(n,vector<int>(target+1,0));
+        vector<int> prev(target+1,0);
+        vector<int> curr(target+1,0);
+        prev[0]=1;
         if(nums[0]<=target){
-            dp[0][nums[0]]=1;
+            prev[nums[0]]=1;
         }
         for(int i=1;i<n;i++){
+            curr[0]=1;
             for(int j=1;j<=target;j++){
                 if(nums[i]<=j){
-                    dp[i][j]=(dp[i-1][j] || dp[i-1][j-nums[i]]);
+                    curr[j]=(prev[j] || prev[j-nums[i]]);
                 }
                 else{
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
                 }
             }
+            prev=curr;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 };
