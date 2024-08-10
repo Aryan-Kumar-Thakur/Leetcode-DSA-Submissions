1efa28ed040b93b336da7ac94da09296bb80159c
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-08-11 02:14:28 +0530

    Time: 20 ms (25.59%), Space: 17.6 MB (29.01%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,25 +1,37 @@
 class Solution {
-    bool solve(int ind,set<string> &st,string &s,int n,vector<int> &dp){
-        if(ind==n){
-            return true;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        bool ans=false;
-        for(int i=ind;i<n;i++){
-            string k=s.substr(ind,i-ind+1);
-            if(st.find(k)!=st.end()){
-                ans=ans||solve(i+1,st,s,n,dp);
-            }
-        }
-        return dp[ind]=ans;
-    }
+    // bool solve(int ind,set<string> &st,string &s,int n,vector<int> &dp){
+    //     if(ind==n){
+    //         return true;
+    //     }
+    //     if(dp[ind]!=-1){
+    //         return dp[ind];
+    //     }
+    //     bool ans=false;
+    //     for(int i=ind;i<n;i++){
+    //         string k=s.substr(ind,i-ind+1);
+    //         if(st.find(k)!=st.end()){
+    //             ans=ans||solve(i+1,st,s,n,dp);
+    //         }
+    //     }
+    //     return dp[ind]=ans;
+    // }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n=s.size();
         set<string> st(wordDict.begin(),wordDict.end());
-        vector<int> dp(n+1,-1);
-        return solve(0,st,s,n,dp);
+        vector<bool> dp(n+1,false);
+        // return solve(0,st,s,n,dp);
+        dp[n]=true;
+        for(int i=n-1;i>=0;i--){
+            bool ans=false;
+            for(int j=i;j<n;j++){
+                string k=s.substr(i,j-i+1);
+                if(st.find(k)!=st.end()){
+                    ans=ans || dp[j+1];
+                }
+            }
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
