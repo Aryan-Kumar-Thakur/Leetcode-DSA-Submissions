991f046b7bdc03b5b566a9737a59a4771bd2e31e
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-08-15 22:30:59 +0530

    Time: 15 ms (80.97%), Space: 16.3 MB (59.43%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,30 +1,26 @@
 class Solution {
-    bool bfs(int node,vector<vector<int>>& graph,vector<int> &setbelong){
-        setbelong[node]=0;
-        queue<int> q;
-        q.push(node);
-        while(!q.empty()){
-            int ele=q.front();
-            q.pop();
-            for(auto it:graph[ele]){
-                if(setbelong[it]==-1){
-                    setbelong[it]=!setbelong[ele];
-                    q.push(it);
-                }
-                else if(setbelong[it]==setbelong[ele]){
+    bool dfs(int node, int parentset, vector<vector<int>>& graph,
+             vector<int>& setbelong) {
+        setbelong[node] = parentset;
+        for (auto it : graph[node]) {
+            if (setbelong[it] == -1) {
+                if (dfs(it, !parentset, graph, setbelong)) {
                     return true;
                 }
+            } else if (setbelong[it] == parentset) {
+                return true;
             }
         }
         return false;
     }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> setbelong(n,-1);
-        for(int i=0;i<n;i++){
-            if(setbelong[i]==-1){
-                if(bfs(i,graph,setbelong)){
+        int n = graph.size();
+        vector<int> setbelong(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (setbelong[i] == -1) {
+                if (dfs(i, 0, graph, setbelong)) {
                     return false;
                 }
             }
