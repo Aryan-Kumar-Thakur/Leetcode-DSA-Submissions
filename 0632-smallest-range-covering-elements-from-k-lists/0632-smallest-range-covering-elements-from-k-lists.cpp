aa68327dbadf9a2730885fb340fd67f4259ba05b
Aryan <125179712+Aryan-Kumar-Thakur@users.noreply.github.com>
2024-10-13 14:51:59 +0530

    Time: 76 ms (16.56%), Space: 24 MB (18.23%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        int n=nums.size();
+        int maxi=INT_MIN;
+        int ans=INT_MAX;
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            pq.push({nums[i][0],0,i});
+            maxi=max(maxi,nums[i][0]);
+        }
+        while(!pq.empty() && pq.size()==n){
+            int mini=pq.top()[0];
+            int ind=pq.top()[1];
+            int arr=pq.top()[2];
+            pq.pop();
+            if(maxi-mini < ans){
+                ans=maxi-mini;
+                res={mini,maxi};
+            }
+            if(ind<nums[arr].size()-1){
+                pq.push({nums[arr][ind+1],ind+1,arr});
+                maxi=max(maxi,nums[arr][ind+1]);
+            }
+        }
+        return res;
+    }
+};
